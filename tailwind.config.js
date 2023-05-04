53e02fe84be36e8a3f35baee659285c5765cb9e8
prathibha077 <prathibhabilagi12@gmail.com>
2023-05-05 00:04:02 +0530

    cosmetic changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
+        
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
         "moi-purple-grad": "linear-gradient(270deg, #9d1fca, #4d2bb9);",
