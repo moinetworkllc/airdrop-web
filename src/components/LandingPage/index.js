a2e432c3cf8c7cfe2acc694acb50ce9a5eae2fa3
prathibha077 <prathibhabilagi12@gmail.com>
2023-05-04 23:54:51 +0530

    added landing page components

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -1,11 +1,75 @@
-import React from "react";
+import React, { useContext } from "react";
+import { classNames } from "../../utils/helpers";
+import { ThemeContext } from "../../context/ThemeContext";
 
 const LandingPage = () => {
+  const { isDarkMode } = useContext(ThemeContext);
   return (
     <>
       <div className="p-auto">
-        <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-          <p className="text-blue-900">Website</p>
+        <div className="flex flex-col justify-center items-center">
+          <p className="bg-moi-gradient inline-block text-transparent text-[71px] bg-clip-text text-center">
+            Personalized Internet
+          </p>
+          <p
+            className={classNames(
+              isDarkMode ? "text-moi-snowfall" : "text-moi-purple-400",
+              "font-semibold text-4xl"
+            )}
+          >
+            Your Network. Your Way.
+          </p>
+          <p
+            className={classNames(
+              isDarkMode ? "text-moi-snowfall" : "text-moi-purple-400",
+              "w-[600px] pt-2 text-center text-lg"
+            )}
+          >
+            MOI is a transformational p2p protocol and an open network that
+            empowers its users to dynamically control their identity, storage
+            and digital assets based on their unique needs.
+          </p>
+        </div>
+        <img className="mx-auto pb-[130px]" src={"/images/moiBg.svg"} />
+        <div className="p-auto flex flex-col justify-start gap-y-[130px]">
+          <div className="flex justify-around">
+            <img src="images/lock.svg" alt="lock" />
+
+            <div
+              className={classNames(
+                isDarkMode ? "text-moi-snowfall" : "text-moi-purple-400",
+                "max-w-[443px] px-12 py-8 dark:bg-card-gradient rounded-lg shadow bg-first-text dark:border-gray-700"
+              )}
+            >
+              <a href="#">
+                <p className="mb-2 text-2xl font-semibold">
+                  We must decentralize
+                </p>
+              </a>
+              <p className="mb-6 font-light text-[22.64px] leading-[145%]">
+                {" "}
+                The Arbitrum DAO is a massive leap forward, making Arbitrum the
+                first EVM rollup tech to reach Stage 1 decentralization.
+              </p>
+              <a
+                href="#"
+                className="inline-flex items-center px-7 py-[9px] text-2xl font-medium text-center bg-button-gradient rounded-md mt-5"
+              >
+                Read more
+              </a>
+            </div>
+          </div>
+          <div className="flex flex-col justify-center items-center">
+            <p className={classNames(
+                isDarkMode ? "text-moi-snowfall" : "text-moi-purple-400", "mt-4 text-[32px] font-semibold not-italic leading-[145%] pb-[72px]")}>
+              Distribution
+            </p>
+            <img
+              src="images/distribution.svg"
+              alt="Distribution"
+              className="w-[676px] h-[584px]"
+            />
+          </div>
         </div>
       </div>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
         "moi-purple-grad": "linear-gradient(270deg, #9d1fca, #4d2bb9);",
+        "moi-gradient": "linear-gradient(91.06deg, #FF1CF7 2.26%, #00F0FF 100%);",
+        "card-gradient": "linear-gradient(180deg, rgba(145, 32, 199, 0.4) 0%, rgba(93, 40, 188, 0.4) 100%)",
+        "button-gradient":"linear-gradient(180deg, #A105EE 0%, #E604DE 100%)"
       },
       colors: {
         "header-bg": "hsla(0,0%,100%,.2)",
