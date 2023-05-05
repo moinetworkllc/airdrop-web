0aae4f38943310d13b11a26b7cf23c8a5376b48b
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2023-05-05 22:57:46 +0530

    adding Footer

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,16 @@
-import { Fragment, useContext } from "react";
-import { Disclosure, Menu, Transition } from "@headlessui/react";
-import { MenuIcon, XIcon } from "@heroicons/react/outline";
-import ToggleComponent from "./Toogle";
-import { classNames } from "../utils/helpers";
-import { ThemeContext } from "../context/ThemeContext";
-import { HeaderTabs } from "../utils/constants";
+import { Fragment, useContext } from 'react';
+import { Disclosure, Menu, Transition } from '@headlessui/react';
+import { MenuIcon, XIcon } from '@heroicons/react/outline';
+import ToggleComponent from './Toogle';
+import { classNames } from '../utils/helpers';
+import { ThemeContext } from '../context/ThemeContext';
+import { HeaderTabs } from '../utils/constants';
 
 export default function Header() {
   const { isDarkMode } = useContext(ThemeContext);
 
   return (
-    <Disclosure
-      as="nav"
-      className={classNames(isDarkMode ? "bg-black" : "bg-moi-white", "shadow")}
-    >
+    <Disclosure as="nav" className={classNames(isDarkMode ? 'bg-black' : 'bg-moi-white', 'shadow')}>
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
@@ -40,10 +37,8 @@ export default function Header() {
                       href={item.href}
                       key={index}
                       className={classNames(
-                        isDarkMode
-                          ? "text-moi-snowfall"
-                          : "text-moi-purple-400",
-                        "inline-flex items-center px-1 pt-1 font-semibold text-md"
+                        isDarkMode ? 'text-moi-snowfall' : 'text-moi-purple-400',
+                        'inline-flex items-center px-1 pt-1 font-semibold text-md',
                       )}
                     >
                       {item.name}
@@ -66,40 +61,6 @@ export default function Header() {
               </div>
             </div>
           </div>
-
-          <Disclosure.Panel className="sm:hidden">
-            <div className="space-y-1 pb-4 pt-2">
-              {/* Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" */}
-              <Disclosure.Button
-                as="a"
-                href="#"
-                className="block border-l-4 border-indigo-500 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-indigo-700"
-              >
-                Dashboard
-              </Disclosure.Button>
-              <Disclosure.Button
-                as="a"
-                href="#"
-                className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700"
-              >
-                Team
-              </Disclosure.Button>
-              <Disclosure.Button
-                as="a"
-                href="#"
-                className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700"
-              >
-                Projects
-              </Disclosure.Button>
-              <Disclosure.Button
-                as="a"
-                href="#"
-                className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700"
-              >
-                Calendar
-              </Disclosure.Button>
-            </div>
-          </Disclosure.Panel>
         </>
       )}
     </Disclosure>
--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -1,6 +1,6 @@
-import React, { useContext } from "react";
-import { classNames } from "../../utils/helpers";
-import { ThemeContext } from "../../context/ThemeContext";
+import React, { useContext } from 'react';
+import { classNames } from '../../utils/helpers';
+import { ThemeContext } from '../../context/ThemeContext';
 
 const LandingPage = () => {
   const { isDarkMode } = useContext(ThemeContext);
@@ -11,45 +11,37 @@ const LandingPage = () => {
           <p className="bg-moi-gradient inline-block text-transparent text-[71px] bg-clip-text text-center">
             Personalized Internet
           </p>
-          <p
-            className={classNames(
-              isDarkMode ? "text-moi-snowfall" : "text-moi-purple-400",
-              "font-semibold text-4xl"
-            )}
-          >
+          <p className={classNames(isDarkMode ? 'text-moi-snowfall' : 'text-moi-purple-400', 'font-semibold text-4xl')}>
             Your Network. Your Way.
           </p>
           <p
             className={classNames(
-              isDarkMode ? "text-moi-snowfall" : "text-moi-purple-400",
-              "w-[600px] pt-2 text-center text-lg"
+              isDarkMode ? 'text-moi-snowfall' : 'text-moi-purple-400',
+              'w-[600px] pt-2 text-center text-lg',
             )}
           >
-            MOI is a transformational p2p protocol and an open network that
-            empowers its users to dynamically control their identity, storage
-            and digital assets based on their unique needs.
+            MOI is a transformational p2p protocol and an open network that empowers its users to dynamically control
+            their identity, storage and digital assets based on their unique needs.
           </p>
         </div>
-        <img className="mx-auto pb-[130px]" src={"/images/moiBg.svg"} />
+        <img className="mx-auto pb-[130px]" src={'/images/moiBg.svg'} />
         <div className="p-auto flex flex-col justify-start gap-y-[130px]">
           <div className="flex justify-around">
             <img src="images/lock.svg" alt="lock" />
 
             <div
               className={classNames(
-                isDarkMode ? "text-moi-snowfall" : "text-moi-purple-400",
-                "max-w-[443px] px-12 py-8 dark:bg-card-gradient rounded-lg shadow bg-first-text dark:border-gray-700"
+                isDarkMode ? 'text-moi-snowfall' : 'text-moi-purple-400',
+                'max-w-[443px] px-12 py-8 dark:bg-card-gradient rounded-lg shadow bg-first-text dark:border-gray-700',
               )}
             >
               <a href="#">
-                <p className="mb-2 text-2xl font-semibold">
-                  We must decentralize
-                </p>
+                <p className="mb-2 text-2xl font-semibold">We must decentralize</p>
               </a>
               <p className="mb-6 font-light text-[22.64px] leading-[145%]">
-                {" "}
-                The Arbitrum DAO is a massive leap forward, making Arbitrum the
-                first EVM rollup tech to reach Stage 1 decentralization.
+                {' '}
+                The Arbitrum DAO is a massive leap forward, making Arbitrum the first EVM rollup tech to reach Stage 1
+                decentralization.
               </p>
               <a
                 href="#"
@@ -60,19 +52,17 @@ const LandingPage = () => {
             </div>
           </div>
           <div className="flex flex-col justify-center items-center">
-            <p className={classNames(
-                isDarkMode ? "text-moi-snowfall" : "text-moi-purple-400", "mt-4 text-[32px] font-semibold not-italic leading-[145%] pb-[72px]")}>
+            <p
+              className={classNames(
+                isDarkMode ? 'text-moi-snowfall' : 'text-moi-purple-400',
+                'mt-4 text-[32px] font-semibold not-italic leading-[145%] pb-[72px]',
+              )}
+            >
               Distribution
             </p>
-            <img
-              src="images/distribution.svg"
-              alt="Distribution"
-              className="w-[676px] h-[584px]"
-            />
+            <img src="images/distribution.svg" alt="Distribution" className="w-[676px] h-[584px]" />
           </div>
-          <div className="flex justify-center">
-            <h1>Card Component</h1>
-          </div>
+          <div className="flex justify-center">{/* <h1>Card Component</h1> */}</div>
         </div>
       </div>
     </>
