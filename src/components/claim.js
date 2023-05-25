43d44ab41a2c6031b3541b0a6ce164b2cef1d4b1
Purva Chaudhari <purvachaudhari@Purvas-MacBook-Pro.local>
2023-05-25 19:24:55 +0530

    signature

--- src/components/claim.js
+++ src/components/claim.js
@@ -1,40 +1,23 @@
-var axios = require('axios');
-const { ethers } = require("ethers");
-const { Network, Alchemy } = require("alchemy-sdk");
-require('dotenv').config();
 
-
-const AUTH='Bearer '+ ""
-const ALCHEMY_API_KEY = ""
-const URL=""
-
-export default function claim() {
-    console.log("IN CLAIM")
-    axios.get("https://api.pinata.cloud/data/testAuthentication",
-    {
-        headers: {
-            'Authorization': AUTH
-        }
-    }).then(response => {
-    console.log(response.data);
-})
-
+export default function getData(moiState, loginData, points) {
+    console.log("Claim airdrop : ",moiState)
+    
 // STEP 2: Upload the Claim data in JSON format and get the CID
 var data = JSON.stringify({
-    "userID": "0xABC",
-    "points": 28500,
+    "userName": loginData.userName,
+    "points": points.total,
     "details": {
         "wallet": {
-            "creation": 1,
+            "creation": points.moid,
             "kyc": {
-                "phone": 2,
-                "email": 2,
-                "id": 5
+                "phone": points.phone_no,
+                "email": points.email,
+                "id": points.kyc
             }
         },
         "validator": {
-            "setup": 20,
-            "early": 10,
+            "setup": points.validator_nodes,
+            "early": points.validator_nodes_may,
             "rewards": [
                 {
                     "kramaID": "Qm...",
@@ -47,50 +30,24 @@ var data = JSON.stringify({
             ]
         },
         "engagement": {
-            "twitter": 10,
-            "telegram": 20,
-            "discord": 30,
-            "transactions": 50
+            "twitter": points.twitter,
+            "telegram": points.telegram,
+            "discord": points.discord,
+            "transactions": points.interactions
         },
         "apps": {
-            "created": 50,
-            "joined": 10
+            "created": points.createdApp,
+            "joined": points.partApp
         },
         "avatars": {
-            "created": 10,
-            "scanned": 5
+            "created": points.createdAvatar,
+            "scanned": points.scannedAvatar
         }
     }
 })
-
-var cid = "";
-axios.post('https://api.pinata.cloud/pinning/pinJSONToIPFS',
-    data,
-    {
-        'Content-Type': 'application/json',
-        headers: {
-            'Authorization': AUTH
-        }
-    }).then(response => {
-    cid = response.data.IpfsHash;
-    console.log(cid);
-    console.log(response.data);
-})
-
-// STEP 3: Sign the CID
-// Load wallet - replace process.env.SEPOLIA_PRIVATE_KEY with MOI ID private key of user
-const wallet = new ethers.Wallet("");
-// Load Alchemy
-const settings = {
-    apiKey: ALCHEMY_API_KEY, // Replace with your Alchemy API Key.
-    network: URL, // Replace with your network.
-};
-const alchemy = new Alchemy(settings);
-// Sign the message
-
-return cid, wallet
-
+   
+  return data
 
 }
 
-export {claim}
+export {getData}
