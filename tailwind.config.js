193f6f19e3acfb0bb4656df7bbc2cfa5814792e5
prathibhabilagi <prathibhabilagi12@gmail.com>
2023-06-10 14:10:59 +0530

    code optimization

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,24 +8,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        "hero-moi": "url('/images/bg-moi-hero.png')",
-        "moi-ball-bg": "url('/images/ball-bg.svg')",
         "hero-section": "url('/images/hero-section.svg')",
-        "moi-eligibility-bg": "url('/images/eligibility-bg.svg')",
-        "moi-discover": "url('/images/discover.svg')",
-        "moi-project": "url('/images/project-icon.svg')",
-        "moi-application": "url('/images/application.svg')",
-        "moi-light-toggle": "url('/images/light-toggle.svg')",
-        "moi-dark-toggle": "url('/images/dark-toggle.svg')",
-        "moi-blur": "url('/images/bg-blur.svg')",
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-        "moi-purple-grad": "linear-gradient(270deg, #9d1fca, #4d2bb9);",
-        "moi-gradient": "linear-gradient(91.06deg, #FF1CF7 2.26%, #00F0FF 100%);",
-        "card-gradient": "linear-gradient(180deg, rgba(145, 32, 199, 0.4) 0%, rgba(93, 40, 188, 0.4) 100%)",
-        "button-gradient": "linear-gradient(180deg, #A105EE 0%, #E604DE 100%)",
-        "moi-button-secondary": "linear-gradient(180deg, #D1D3E2 0%, #75629D 100%)",
-        "eligibility-gradient": "linear-gradient(180deg, rgba(145, 32, 199, 0.1) 0%, rgba(93, 40, 188, 0.1) 100%)",
         "main-bg": "radial-gradient(at 47% 33%, hsl(240.00, 35%, 36%) 0, transparent 39%), radial-gradient(at 22% 65%, rgba(10, 0, 38, 0.84) 0, transparent 55%)"
       },
       boxShadow: {
